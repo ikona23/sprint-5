efe9b213f9df4ef39a35d04fe25f24d0cd838b26
Peter Kona <ikona23@gmail.com>
2016-07-29 03:34:24 +1200

    exercise-3

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,2 +1,39 @@
-  for (var number = "#"; number.length <= 7; number += "#")
-  console.log(number);
+// Exercise 1
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+  for (var i = "#"; i.length <= 7; i += "#")
+  console.log(i);
+
+// Exercise 2
+  for (var i = 1; i <= 100; i++) {
+  if (i%3 == 0 && i%5 == 0) {
+  console.log("fizzBuzz");
+  }else if (i % 3 == 0) {
+  console.log("fizz");
+  }else if (i % 5 == 0) {
+  console.log("buzz");
+  } else {
+    console.log(i);
+  }
+};
+
+//Exercise 3
+var size = 8;
+var space = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0){
+      space += " ";
+  }  else {
+      space += "#";
+  }
+  space += "\n";
+}
+console.log(space);
